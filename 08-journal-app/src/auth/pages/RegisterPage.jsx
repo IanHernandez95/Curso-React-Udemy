1b10a8fd8c26f8190ded97eeb8c4cc25efa6e160
IanHernandez95 <ian.hernandez.f@gmail.com>
2022-11-28 13:04:41 -0300

    Creaccion Journal App-Ejercicio 08

---
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -0,0 +1,59 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from '../layout/AuthLayout'
+
+
+export const RegisterPage = () => {
+    return (
+        <AuthLayout title='Register'>
+            
+            <form>
+                    <Grid container>
+                        <Grid item xs={ 12 } sx={{ mt: 2}}>
+                            <TextField 
+                                label='Nombre' 
+                                type='text' 
+                                placeholder='Nombre Completo'
+                                fullWidth 
+                            />
+                        </Grid>
+                        <Grid item xs={ 12 } sx={{ mt: 2}}>
+                            <TextField 
+                                label='Correo' 
+                                type='email' 
+                                placeholder='Correo@example.com'
+                                fullWidth 
+                            />
+                        </Grid>
+                        <Grid item xs={ 12 } sx={{ mt: 2}}>
+                            <TextField 
+                                label='Contraseña' 
+                                type='password' 
+                                placeholder='Contraseña'
+                                fullWidth 
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={ 12 }>
+                            <Button variant="contained" fullWidth>
+                                Crear Cuenta
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container direction='row' justifyContent='end'>
+                        <Typography>¿Ya tienes cuenta? </Typography>
+                        <Link component={ RouterLink } color='inherit' to='/auth/login' sx={{ ml: 1}}>
+                            Ingresar
+                        </Link>
+
+                    </Grid>
+
+                </form>
+
+        </AuthLayout>
+        
+    )
+}
---
+++ 08-journal-app/src/journal/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import { LogoutOutlined, MenuOutlined } from "@mui/icons-material"
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material"
+
+
+export const NavBar = ({ drawerWith = 240}) => {
+    return (
+        <AppBar 
+            position="fixed"
+            sx={{
+                width: { sm: `calc(100% - ${ drawerWith}px)` },
+                ml: { sm: `${ drawerWith}px`}
+            }}
+        >
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    sx={{ mr:2, display: { sm: 'none' }}}
+                >
+                    <MenuOutlined/>
+                </IconButton>
+
+                <Grid container direction='row' justifyContent='space-between' alignItems='center'>
+                    <Typography variant="h6" noWrap> Journal App</Typography>
+
+                    <IconButton color="error">
+                        <LogoutOutlined/>
+                    </IconButton>
+
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
+}
