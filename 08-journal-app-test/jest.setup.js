8ca31d0d1650d7beca2bd28935f57a362795abd7
IanHernandez95 <ian.hernandez.f@gmail.com>
2022-12-14 19:17:09 -0300

    Testing Journal App

---
+++ 08-journal-app-test/jest.setup.js
@@ -0,0 +1,12 @@
+// En caso de necesitar la implementación del FetchAPI
+import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+import 'setimmediate';
+jest.setTimeout(30000);
+
+require('dotenv').config({
+    path:'.env.test'
+});
+
+jest.mock('./src/helpers/getEnvironments', () => ({
+    getEnvironments: () => ({ ...process.env})
+}))
