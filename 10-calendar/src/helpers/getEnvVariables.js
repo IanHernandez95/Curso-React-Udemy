609e16eaa3719f63226b12e4677ea9914521f442
IanHernandez95 <ian.hernandez.f@gmail.com>
2022-12-26 13:46:06 -0300

    correcciones en el 10-calendar!

--- 10-calendar/src/helpers/getEnvVariables.js
+++ 10-calendar/src/helpers/getEnvVariables.js
@@ -2,11 +2,11 @@
 
 export const getEnvVariables = () => {
 
-    import.meta.env;
+    // import.meta.env;
 
     return {
-        // VITE_API_URL: import.meta.env.VITE_API_URL,
-        ...import.meta.env
+        VITE_API_URL: import.meta.env.VITE_API_URL,
+        // ...import.meta.env
     }
 
 }
